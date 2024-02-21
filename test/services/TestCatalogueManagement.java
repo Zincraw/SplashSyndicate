6736c7ebd73b57fde225a196f06d4280f5fb9090
r <raul8gb@gmail.com>
2024-02-21 17:37:30 +0100

    Unit Test added

---
+++ test/services/TestCatalogueManagement.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.assertEquals;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+import data.Catalogue;
+
+public class TestCatalogueManagement {
+	
+	@Test
+	public void isGameLoadEqualsToCSVLinesPU01() {
+		Catalogue catalogue = new Catalogue();
+		int numberOfLines = 0;
+		
+		Path path = Paths.get("src/resources/vgsales.csv");
+		
+		try {
+			numberOfLines = (int) Files.lines(path).count();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		//if ((numberOfLines + 1) == catalogue.getMap().size())) System.out.println("asfdasf");
+		assertEquals(numberOfLines + 1, catalogue.getMap().size());
+	}
+}
