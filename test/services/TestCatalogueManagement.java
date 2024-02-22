f01a3a3fc206371767818f7c65c9176118480b37
r <raul8gb@gmail.com>
2024-02-22 16:22:24 +0100

    PU02 completed

--- test/services/TestCatalogueManagement.java
+++ test/services/TestCatalogueManagement.java
@@ -2,6 +2,8 @@ package services;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -28,19 +30,35 @@ public class TestCatalogueManagement {
 		assertEquals(numberOfLines, catalogue.getMap().size() + 1);
 	}
 	
+	//Simulation of unit test which returns false
+//	@Test
+//	public void isGameLoadedCorrectlyFalse() {
+//		Catalogue catalogue = new Catalogue();
+//		int numberOfLines = 0;
+//		
+//		Path path = Paths.get("src/resources/vgsales.csv");
+//		
+//		try {
+//			numberOfLines = (int) Files.lines(path).count();
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//		}
+//		
+//		assertEquals(numberOfLines, catalogue.getMap().size());
+//	}
+	
 	@Test
-	public void isGameLoadedCorrectlyFalse() {
-		Catalogue catalogue = new Catalogue();
-		int numberOfLines = 0;
+	public void isPrintAllCatalogueCorrectlyTrue() {
+		CatalogueManagement catalogueManagement = new CatalogueManagement();
+
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream ();
 		
-		Path path = Paths.get("src/resources/vgsales.csv");
-		
-		try {
-			numberOfLines = (int) Files.lines(path).count();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		
-		assertEquals(numberOfLines, catalogue.getMap().size());
+		System.setOut(new PrintStream(outContent));
+		catalogueManagement.printAllCatalogue();		
+
+		int printedGamesCount = outContent.toString().split("\n").length / 7;
+		int CatalogueGamesCount = catalogueManagement.getCatalogue().getData().size();
+		assertEquals(printedGamesCount, CatalogueGamesCount );
 	}
+	
 }
