c5f7425290069c744f1691fa420c7a2d9e9655c8
Xavier Guillamón <xaviergs99@gmail.com>
2024-02-22 16:33:18 +0100

    Points 4 and 5 done, + refactoring

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -2,10 +2,6 @@ package controller;
 
 public class Main {
 	public static void main(String[] args) {
-		
-		//Test.testGameClass();
-		//Test.testCatalogueClass();
-		//Test.testCatalogueClass();
 		Menu menu = new Menu();
         menu.start();
 	}
